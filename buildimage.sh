3b9427487a91e975cd16084e7d302f766c0f610a
Rakib Ansary <rakibansary@gmail.com>
2023-03-22 00:06:57 +0600

    fix: bump version

--- buildimage.sh
+++ buildimage.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:de77b2c82be50d851701139201a7d50f46f909d8
 informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:3970668f68bdde0c86aca79e5e4fefc51f2e10cc
 anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:1441e4da7be6ca4492be99bf69a123b5a7781713
-domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:a12455159a9fc6822d8cb7fde7031bd817b210d8
+domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:c71e661fc400cc48144c2c44e7427203f758f034
 
 sed -i='' "s|dynamo-access-layer:latest|$dynamo_access_layer_TAG|" docker-compose.yml
 
