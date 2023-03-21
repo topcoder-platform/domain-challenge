883ea01cc6b315fe7b22573a9d1043fd89bc2a9a
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2023-03-21 12:10:52 +0530

    Code change for deployment

--- buildimage.sh
+++ buildimage.sh
@@ -7,11 +7,11 @@ anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/antic
 domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:7ebc2092935c541ad4a12a4089578fd914929e71
 
 sed -i='' "s|dynamo-access-layer:latest|$dynamo_access_layer_TAG|" docker-compose.yml
-sleep 10
+
 sed -i='' "s|informix-access-layer:latest|$informix_access_layer_TAG|" docker-compose.yml
-sleep 10
+
 sed -i='' "s|anticorruption-layer:latest|$anticorruption_layer_TAG|" docker-compose.yml
-sleep 10
+
 sed -i='' "s|domain-challenge_TAG:latest|$domain_challenge_TAG|" docker-compose.yml
-sleep 10
+
 
