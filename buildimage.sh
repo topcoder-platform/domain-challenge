5f97e3cf2faf35ca03b291e2e2bcd36bb5c4ba07
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2023-03-21 11:47:20 +0530

    Code change for deployment

---
+++ buildimage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eo pipefail
+
+dynamo-access-layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:de77b2c82be50d851701139201a7d50f46f909d8
+informix-access-layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:3970668f68bdde0c86aca79e5e4fefc51f2e10cc
+anticorruption-layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:4706f8b8aced8bef5f74d59ec94726ff8d3e6b44
+domain-challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:7ebc2092935c541ad4a12a4089578fd914929e71
+
+sed -i='' "s|dynamo-access-layer:latest|$dynamo-access-layer_TAG|" docker-compose.yml
+sed -i='' "s|informix-access-layer:latest|$informix-access-layer_TAG|" docker-compose.yml
+sed -i='' "s|anticorruption-layer:latest|$anticorruption-layer_TAG|" docker-compose.yml
+sed -i='' "s|domain-challenge_TAG:latest|$domain-challenge_TAG|" docker-compose.yml
+
