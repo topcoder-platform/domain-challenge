d3d5b92168c66ac1a63ff1c193ae62224f134453
Thomas Kranitsas <thomas.kranitsas@topcoder.com>
2023-03-21 13:19:15 +0200

    remove JSON.stringify from prizeSets

--- src/domain/Challenge.ts
+++ src/domain/Challenge.ts
@@ -1274,9 +1274,9 @@ class ChallengeDomain extends CoreOperations<Challenge, CreateChallengeInput> {
 
     const challengeList = await super.update(scanCriteria, {
       ..._.omit(input, ["id", ...(!input.legacyId ? ["legacyId"] : [])]),
-      ...(input.prizeSets && input.prizeSets.length > 0
-        ? { prizeSets: input.prizeSets.map((ps) => JSON.stringify(ps)) }
-        : {}),
+      // ...(input.prizeSets && input.prizeSets.length > 0
+      //   ? { prizeSets: input.prizeSets.map((ps) => JSON.stringify(ps)) }
+      //   : {}),
     });
 
     console.log("------ after save --------");
