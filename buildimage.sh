e99c4016b06f5731dd9adc8d52b4d9d0e30dfbaf
Rakib Ansary <rakibansary@gmail.com>
2023-03-21 18:54:52 +0600

    ci: deploy to dev

--- buildimage.sh
+++ buildimage.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:de77b2c82be50d851701139201a7d50f46f909d8
 informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:3970668f68bdde0c86aca79e5e4fefc51f2e10cc
 anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:ff5ea702a418e8e27d458c62df6ccf8d33c8b6a7
-domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:860fdb70210c66781cd78b73e58de7887492d7e1
+domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:a12455159a9fc6822d8cb7fde7031bd817b210d8
 
 sed -i='' "s|dynamo-access-layer:latest|$dynamo_access_layer_TAG|" docker-compose.yml
 
