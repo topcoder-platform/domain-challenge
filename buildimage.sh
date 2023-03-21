39b14f03697d95da7a92341781b03a5a96b4084c
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2023-03-21 16:30:58 +0530

    Code change for deployment

--- buildimage.sh
+++ buildimage.sh
@@ -3,8 +3,8 @@ set -eo pipefail
 
 dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:de77b2c82be50d851701139201a7d50f46f909d8
 informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:3970668f68bdde0c86aca79e5e4fefc51f2e10cc
-anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:ad933bd67864a8fc49f310e0600096ee0ff79547
-domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:e6d92cf91c48f7aba323bc42f55c3a042ad2a329
+anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:b0e3f9223f62be1197c69ebe87ad419169c4f0dc
+domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:395f6b05794f5136f8e73cd4061c4fb3c8f76a52
 
 sed -i='' "s|dynamo-access-layer:latest|$dynamo_access_layer_TAG|" docker-compose.yml
 
