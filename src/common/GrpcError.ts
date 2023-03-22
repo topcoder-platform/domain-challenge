baf5bef236ca29f667b8055e6d727f338bb77088
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2023-03-22 14:59:44 +0530

    prod changes [skip ci]

--- src/common/GrpcError.ts
+++
@@ -1,7 +0,0 @@
-import { StatusObject } from "@grpc/grpc-js";
-
-export class GrpcError extends Error {
-  constructor(public error: Partial<StatusObject>) {
-    super(error.details);
-  }
-}
--- src/helpers/IdGenerator.ts
+++
@@ -1,9 +0,0 @@
-import { uuid } from "uuidv4";
-
-class IdGenerator {
-  public generateUUID(): string {
-    return uuid();
-  }
-}
-
-export default new IdGenerator();
