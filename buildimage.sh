0aaba59a0e66c5f1f296f3afdf88f2d2a8977846
Rakib Ansary <rakibansary@gmail.com>
2023-03-25 21:13:36 +0600

    fix: f2f or crash

--- buildimage.sh
+++ buildimage.sh
@@ -5,7 +5,7 @@ ENV=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
 
 dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:77edc76a80f75e734b0e88d9df22e833a7669ac4
 informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:16ef377f47cce7eca0bd6f388a41a94a1b33dbd8
-anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:5a32add8fc1e64dda0606abc72e95c98b75560e7
+anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:0a7a52348a2120b32f94fe94e531bd5d7053aafe
 domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:4fe0db5f8111ca59e3fdd26f359e6def708d58bf
 
 sed -i='' "s|dynamo-access-layer:latest|$dynamo_access_layer_TAG|" docker-compose.yml
