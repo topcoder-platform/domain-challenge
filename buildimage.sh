305aeb200c2bc67270d435610bca70ed5003d6b2
Rakib Ansary <rakibansary@gmail.com>
2023-03-26 22:16:00 +0600

    fix: prize undefined
    Signed-off-by: Rakib Ansary <rakibansary@gmail.com>

--- buildimage.sh
+++ buildimage.sh
@@ -5,8 +5,8 @@ ENV=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
 
 dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:77edc76a80f75e734b0e88d9df22e833a7669ac4
 informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:750d946d64de240f6ec5b5fc3d399ea363f312d4
-anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:bc49448415bb740b0068d2b34c8fb00799880ea2
-domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:aecdc664372bef8b20c5667ccd4e2758edae610f
+anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:156445c6429e499262b0ae25a96ae3f1ac7ee655
+domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:29662c8e5ae788fd229c4eea2a16324198573e51
 
 sed -i='' "s|dynamo-access-layer:latest|$dynamo_access_layer_TAG|" docker-compose.yml
 
