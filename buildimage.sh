4ecb01e763720ddfc3651eabf831a9f7bfe52d96
Rakib Ansary <rakibansary@gmail.com>
2023-04-07 09:24:03 +0600

    ci: deploy schema, prize sets fixes
    Signed-off-by: Rakib Ansary <rakibansary@gmail.com>

--- buildimage.sh
+++ buildimage.sh
@@ -4,9 +4,9 @@ ENV=$1
 ENV=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
 
 informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:99824c2c8cfdd60011fb4bec876f73c7821ef6f8
-dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:206f4e5c99e6b98c510b45b3fe4f534d661ac78c
-anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:8db13cbdd49ac17d406dd5b12e2c5ca2c3c90f28
-domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:7609b06042ab995641f20807078deb8595988341
+dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:2b25428b69398c50baef636cf6833a8094a97281
+anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:83e293e22f76f87a690d0f61c967d86bc264f7f1
+domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:fd81e19391f4273f00af72f2ec3890422016b258
 
 sed -i='' "s|dynamo-access-layer:latest|$dynamo_access_layer_TAG|" docker-compose.yml
 
