00a7e6c2a4fb479b51198e470810344b565df4a0
Rakib Ansary <rakibansary@gmail.com>
2023-04-14 14:18:45 +0600

    fix: loss of precesion when converting float to integer
    Signed-off-by: Rakib Ansary <rakibansary@gmail.com>

--- buildimage.sh
+++ buildimage.sh
@@ -7,7 +7,7 @@ ENV=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
 
 informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:88ce1fe027ec480e00a061e42eddcda6fcc9d42c
 dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:c68e60e837eb3284d289a5a85e8089a677b5cf93
-anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:cc2e9ad4aa117e11b20f50becb6fdbde939b7cda
+anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:9acb402bdc4230f4bb24c5afa3faff1953356756
 domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:55dc29cf97339d507792eff9cd63744ef51fa304
 
 sed -i='' "s|dynamo-access-layer:latest|$dynamo_access_layer_TAG|" docker-compose.yml
