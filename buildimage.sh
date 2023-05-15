5d1002a257d95ebd1f5fb58f70b18834a3b9c9c5
Rakib Ansary <rakibansary@gmail.com>
2023-05-15 15:48:50 +0600

    ci: plat-2772
    Signed-off-by: Rakib Ansary <rakibansary@gmail.com>

--- buildimage.sh
+++ buildimage.sh
@@ -6,9 +6,9 @@ ENV=$1
 ENV=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
 
 informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:f0543ec7f37d974f198815494a9c5f1f374cc232
-dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:2b25428b69398c50baef636cf6833a8094a97281
-anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:be1373e3aabd3641ce505dfad741a6b7e5d9caef
-domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:06704dc144c09064b9f260bc480651f772db8262
+dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:837f670f24ab9a2572036987c16fa585d9262c81
+anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:017d573a780e14a55bd1db0e3ea18df2c4d7126a
+domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:f5e917e7b07b9be608c450897f283c74a47cebee
 
 sed -i='' "s|dynamo-access-layer:latest|$dynamo_access_layer_TAG|" docker-compose.yml
 
