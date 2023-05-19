a6b9bfdd976ff77d1c64bbb412b4d6897f46164c
Rakib Ansary <rakibansary@gmail.com>
2023-05-19 15:47:27 +0600

    ci: deploy to qa

--- buildimage.sh
+++ buildimage.sh
@@ -6,8 +6,8 @@ ENV=$1
 ENV=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
 
 informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:28ead7341cf8057cc5d614d3686dac97aa5d77e9
-dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:28ead7341cf8057cc5d614d3686dac97aa5d77e9
-anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:28ead7341cf8057cc5d614d3686dac97aa5d77e9
+dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:368230ca577920f29c8710acd8d84406a93ab2e3
+anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:4070b759fe92755aae3d699bc97b0b96bcc96ec0
 domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:ade07caf6c129104edaf72c94997408f2840eb16
 
 sed -i='' "s|dynamo-access-layer:latest|$dynamo_access_layer_TAG|" docker-compose.yml
