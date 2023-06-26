5f7d7885526d77aac4d0ccba998e7a519bcb3a1e
Rakib Ansary <rakibansary@topcoder.com>
2023-06-26 16:44:36 +0600

    ci: deploy to dev
    Signed-off-by: Rakib Ansary <rakibansary@topcoder.com>

--- buildimage.sh
+++ buildimage.sh
@@ -7,7 +7,7 @@ ENV=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
 
 informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:70a0bade6a649f7c8e2995ffe2fe2f46a0de6a97
 dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:024afa71805187e3780a0865c0d755f73dccda90
-anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:b48f2796b337d790946c98817f50c7078ae1aff5
+anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:76b6e6abf5dca2c62e0c37108d6be0d73c753d59
 domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:04fb7567b963db336e83c29c9a5fc91a5a1c954c
 
 sed -i='' "s|dynamo-access-layer:latest|$dynamo_access_layer_TAG|" docker-compose.yml
