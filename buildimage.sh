6b3523b8c93dcc303394725d96354d82f18bd606
Rakib Ansary <rakibansary@topcoder.com>
2023-07-03 11:38:17 +0600

    ci: deploy to dev
    Signed-off-by: Rakib Ansary <rakibansary@topcoder.com>

--- buildimage.sh
+++ buildimage.sh
@@ -5,9 +5,9 @@ set -eo pipefail
 ENV=$1
 ENV=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
 
-informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:70a0bade6a649f7c8e2995ffe2fe2f46a0de6a97
+informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:9dfb08c426602bbc292ac824a326b89a1e6dadfc
 dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:024afa71805187e3780a0865c0d755f73dccda90
-anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:76b6e6abf5dca2c62e0c37108d6be0d73c753d59
+anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:e633baf8d66ced64077ffe971a69fcf7d95bc148
 domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:04fb7567b963db336e83c29c9a5fc91a5a1c954c
 
 sed -i='' "s|dynamo-access-layer:latest|$dynamo_access_layer_TAG|" docker-compose.yml
