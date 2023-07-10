7465aa17dfb31fb0690a55fa10de4c1181a83a03
Rakib Ansary <rakibansary@topcoder.com>
2023-07-10 13:58:49 +0600

    fix: add challenge completion timestamp (#52)
    Signed-off-by: Rakib Ansary <rakibansary@topcoder.com>

--- src/util/DateUtil.ts
+++ src/util/DateUtil.ts
@@ -1,19 +1,20 @@
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
+
 import timezone from "dayjs/plugin/timezone";
+import advanced from "dayjs/plugin/advancedFormat";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(advanced);
 
 export const IFX_TIMEZONE = "America/New_York";
-export const dateFormatIfx = "YYYY-MM-DD HH:mm:ss";
+export const IFX_DATE_FORMAT = "YYYY-MM-DD HH:mm:ss";
 
 class DateUtil {
-  public formatDateForIfx(date: string): string | undefined {
+  public formatDateForIfx(date: string, format: string = IFX_DATE_FORMAT): string | undefined {
     const parsedDate = dayjs(date);
-    return parsedDate.isValid()
-      ? parsedDate.tz(IFX_TIMEZONE).format(dateFormatIfx)
-      : undefined;
+    return parsedDate.isValid() ? parsedDate.tz(IFX_TIMEZONE).format(format) : undefined;
   }
 
   public isValidDate(date: string) {
