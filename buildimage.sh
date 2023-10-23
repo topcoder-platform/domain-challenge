c98221a7f2d68fd1b0c6364814f325422e37d6e9
Rakib Ansary <rakibansary@topcoder.com>
2023-10-23 13:15:21 +0600

    fix: deploy PLAT-3491

--- buildimage.sh
+++ buildimage.sh
@@ -5,10 +5,10 @@ set -eo pipefail
 ENV=$1
 ENV=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
 
-informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:691f81e60c13c307928355e1e5108ab6fc8459c5
-dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:68721c33a1cdcbffe48c3c3400d8d1721ad8a395
-anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:691f81e60c13c307928355e1e5108ab6fc8459c5
-domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:b3272bc853a77a3c496cd5070b550ec52126230d
+informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:70f10ddc7c172dce915293f5b2ae38ca9bd995c7
+dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:70013a9442c79b45750e4fc0c640488e8f2cf2bb
+anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:ddbb3f484ae4836fd82c9cfb8033b08fd2599e83
+domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:5b109bd58b042a1e9bd6c141c828925f77a41057
 
 sed -i='' "s|dynamo-access-layer:latest|$dynamo_access_layer_TAG|" docker-compose.yml
 
