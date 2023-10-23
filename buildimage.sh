0929f91313a7a405f99399b92c5f0fb084e7fa76
Rakib Ansary <rakibansary@topcoder.com>
2023-10-23 17:56:13 +0600

    ci: PLAT-3491 - generate payments for tasks
    Signed-off-by: Rakib Ansary <rakibansary@topcoder.com>

--- buildimage.sh
+++ buildimage.sh
@@ -8,7 +8,7 @@ ENV=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
 informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:70f10ddc7c172dce915293f5b2ae38ca9bd995c7
 dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:9818e01a468eb0ba6e20bd6e7f0ca0b160358283
 anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:de7c5286c593663297ebc088a7b24da9a0c4a3c8
-domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:0e624320bd86d05aa363e7a32807353815ea712f
+domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:8e7af8edf88d1996a4caa785e51f293113654832
 
 sed -i='' "s|dynamo-access-layer:latest|$dynamo_access_layer_TAG|" docker-compose.yml
 
