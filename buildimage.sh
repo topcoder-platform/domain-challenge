833a3ec9afa501e350a10304e4a13d696a106e20
Rakib Ansary <rakibansary@topcoder.com>
2023-11-13 20:48:19 +0600

    ci: PLAT-3491

--- buildimage.sh
+++ buildimage.sh
@@ -7,7 +7,7 @@ ENV=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
 
 informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:a9e552738879d6ce31079fd198ec5289aabff540
 dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:8b61b8a3c160b3b03eeb4aee48eb869b3c5e110c
-anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:4eb2660b6e1214a2f070cdcee85c9e29b0deab60:
+anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:4eb2660b6e1214a2f070cdcee85c9e29b0deab60
 domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:9eacfedcda1548b68f3d4b001530e277b16e8002
 
 sed -i='' "s|dynamo-access-layer:latest|$dynamo_access_layer_TAG|" docker-compose.yml
