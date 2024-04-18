906f1322c7e4abb3a47b56aefb45ee8525d49e52
Justin Gasper <jmgasper@gmail.com>
2024-04-19 08:18:02 +1000

    Fix for CORE-403 where phase changes not propagating to DynamoDB

--- buildimage.sh
+++ buildimage.sh
@@ -6,7 +6,7 @@ ENV=$1
 ENV=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
 
 informix_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/informix-access-layer:73812d40e5867bc6a78cf45c35c57e5a4e51cac8
-dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:20cd1b16a4d1ac171d6f09e3f7cdffa4dbc5640f
+dynamo_access_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/dynamo-access-layer:117d44ee1a9f04383980bb20429d31e871589a47
 anticorruption_layer_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/anticorruption-layer:2ea3600c121cf7bc09202d7c09953e71c9485fb3
 domain_challenge_TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/domain-challenge:f03939cbfbed8a9bd132a6be3616565a820c4e44
 
